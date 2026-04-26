60bc6e8985d41b394f2050c213a4990edd2ba3c5
alidrew <ayalaalisson893@gmail.com>
2026-04-26 15:52:02 -0500

    filtros y ordenamiento - planeamiento y reclutamiento

--- src/app/pages/planeamiento/planeamiento.ts
+++ src/app/pages/planeamiento/planeamiento.ts
@@ -1,20 +1,70 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { RqService } from '../../services/rq';
 
 @Component({
   selector: 'app-planeamiento',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './planeamiento.html',
   styleUrl: './planeamiento.css'
 })
 export class Planeamiento {
   rqs: any[] = [];
+  statusFilter: string = 'todos';
+  campaignFilter: string = '';
+  sortColumn: string = '';
+  sortDirection: 'asc' | 'desc' = 'asc';
 
   constructor(private rqService: RqService) {
     this.rqs = this.rqService.getAll();
+  }
+
+  sortBy(column: string) {
+    if (this.sortColumn === column) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortColumn = column;
+      this.sortDirection = 'asc';
+    }
+  }
+
+  get filteredAndSortedRqs() {
+    let filtered = this.rqs;
+
+    if (this.statusFilter !== 'todos') {
+      filtered = filtered.filter(rq => rq.estado.toLowerCase() === this.statusFilter);
+    }
+
+    if (this.campaignFilter) {
+      filtered = filtered.filter(rq => rq.campaign === this.campaignFilter);
+    }
+
+    if (this.sortColumn) {
+      filtered.sort((a, b) => {
+        let aVal = a[this.sortColumn];
+        let bVal = b[this.sortColumn];
+        let result = 0;
+
+        if (this.sortColumn === 'fechaInicioCapacitacion' || this.sortColumn === 'fechaFinCapacitacion' || this.sortColumn === 'fechaIngreso') {
+          result = new Date(aVal).getTime() - new Date(bVal).getTime();
+        } else if (this.sortColumn === 'cantidad') {
+          result = aVal - bVal;
+        } else {
+          result = aVal.localeCompare(bVal);
+        }
+
+        return this.sortDirection === 'asc' ? result : -result;
+      });
+    }
+
+    return filtered;
+  }
+
+  get uniqueCampaigns() {
+    return [...new Set(this.rqs.map(rq => rq.campaign))];
   }
 
   getTotalRQs() {
@@ -23,6 +73,10 @@ export class Planeamiento {
 
   getActiveRQs() {
     return this.rqs.filter(rq => rq.estado === 'ABIERTO').length;
+  }
+
+  getTotalPersonalRequerido() {
+    return this.rqs.filter(rq => rq.estado === 'ABIERTO').reduce((sum, rq) => sum + rq.cantidad, 0);
   }
 
   getTotalPostulantes() {
